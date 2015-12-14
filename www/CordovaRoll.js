29a91c223900d8cf93081f01d98229dd7731d979
egmfilho <edumig12@hotmail.com>
2015-12-14 20:28:28 -0200

    nada

--- www/CordovaRoll.js
+++ www/CordovaRoll.js
@@ -7,7 +7,6 @@ module.exports = {
         } else if (typeof failureCallback != 'function') {
             console.log('CordovaRoll Error: failureCallback is not a function!');
         } else {
-
             var data = String(dataURL).replace(/data:image\/png;base64,/,'');
 
             return cordova.exec(successCallback, failureCallback, 'CordovaRoll', 'saveToCameraRoll', [data]);
